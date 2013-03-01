163dcdce23df20d7e9a0dce454ed9576566ddd7e
d3v11 <d3v11@d3v11.ano>
2013-03-01 01:01:58 +0000

    [Make.sh] fixes (gcc -O sucks)

--- Make.sh
+++ Make.sh
@@ -4,5 +4,7 @@
   echo 'mv /usr/lib/libnacl.so /usr/lib/breaksBC.so' 1>&2 && \
   exit 64
 
-gcc -O2 src/pubkey.c -o pubkey -l nacl
-gcc -O2 src/cryptotun.c -o cryptotun -l nacl /usr/lib/randombytes.o
+# WARNING: setting gcc -O* breaks taia
+
+gcc src/pubkey.c -o pubkey -l nacl
+gcc src/cryptotun.c -o cryptotun -l nacl /usr/lib/randombytes.o
