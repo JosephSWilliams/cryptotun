95ec0a8b310281f7262d098e83df1517937c0867
d3v11 <d3v11@d3v11.ano>
2013-03-01 02:11:22 +0000

    [Make.sh] try again with optimization now that we have a good solution for taia

--- Make.sh
+++ Make.sh
@@ -4,5 +4,5 @@
   echo 'mv /usr/lib/libnacl.so /usr/lib/breaksBC.so' 1>&2 && \
   exit 64
 
-gcc src/pubkey.c -o pubkey -l nacl
-gcc src/cryptotun.c -o cryptotun -l tai -l nacl /usr/lib/randombytes.o
+gcc -O3 src/pubkey.c -o pubkey -l nacl
+gcc -O3 src/cryptotun.c -o cryptotun -l tai -l nacl /usr/lib/randombytes.o
