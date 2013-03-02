fc1924b0f1806473de3a58cf948c30a1994dbb67
d3v11 <d3v11@d3v11.ano>
2013-03-02 05:18:28 +0000

    added a conf-cc

--- Make.sh
+++ Make.sh
@@ -4,5 +4,5 @@
   echo 'mv /usr/lib/libnacl.so /usr/lib/breaksBC.so' 1>&2 && \
   exit 64
 
-gcc -O3 src/pubkey.c -o pubkey -l nacl
-gcc src/cryptotun.c -o cryptotun -l tai -l nacl /usr/lib/randombytes.o
+gcc `cat conf-cc` src/pubkey.c -o pubkey -l nacl
+gcc `cat conf-cc` src/cryptotun.c -o cryptotun -l tai -l nacl /usr/lib/randombytes.o
