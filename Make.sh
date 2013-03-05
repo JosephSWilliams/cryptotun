fc64608a32b81b1eb11baa02522d6515f1bd1c88
d3v11 <d3v11@d3v11.ano>
2013-03-05 02:51:54 +0000

    [Make.sh, README] try to fix bsd; added libtai to dependency list

--- Make.sh
+++ Make.sh
@@ -4,5 +4,8 @@
   echo 'mv /usr/lib/libnacl.so /usr/lib/breaksBC.so' 1>&2 && \
   exit 64
 
-gcc `cat conf-cc` src/pubkey.c -o pubkey -l nacl
-gcc `cat conf-cc` src/cryptotun.c -o cryptotun -l tai -l nacl /usr/lib/randombytes.o
+[ `uname -s` == OpenBSD ] && CFLAGS=-DPOSIX_SOURCE
+[ `uname -s` == FreeBSD ] && CFLAGS=-DPOSIX_SOURCE
+
+gcc `cat conf-cc` $CFLAGS src/pubkey.c -o pubkey -l nacl
+gcc `cat conf-cc` $CFLAGS src/cryptotun.c -o cryptotun -l tai -l nacl /usr/lib/randombytes.o
