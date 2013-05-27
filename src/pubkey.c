7b4dbf83fb4051e4bbd60afe7fe4a9a80200459c
d3v11 <d3v11@d3v11.ano>
2013-05-27 02:52:50 +0000

    [pubkey.c] less

--- src/pubkey.c
+++ src/pubkey.c
@@ -1,21 +1,21 @@
 #define USAGE "Usage: pubkey /path/to/seckey\n"
 #include <nacl/crypto_scalarmult_curve25519.h>
-#include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-main(int argc, char **argv){
-if (argc>1){
-  int i;
-  i = open(argv[1],0);
-  if (i<0) exit(64);
-  unsigned char pk[32], sk[32];
-  if (read(i,sk,32)<32) exit(64);
-  close(i);
+
+int main(int argc, char **argv) {
+ if (argc>1) {
+  unsigned char pk[32];
+  unsigned char sk[32];
+  int n;
+  if (((n=open(argv[1],0))<0)||(read(n,sk,32)<32)||(close(n)<0)) return 64;
   crypto_scalarmult_curve25519_base(pk,sk);
   memset(sk,0,32);
   printf("pubkey: ");
-  for (i=0;i<32;++i) printf("%02x",pk[i]);
+  for (n=0;n<32;++n) printf("%02x",pk[n]);
   printf("\n");
-  exit(0);}
-write(2,USAGE,strlen(USAGE));
-exit(64);}
+  return 0;
+ }
+ write(2,USAGE,strlen(USAGE));
+ return 64;
+}
