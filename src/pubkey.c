f12e4ef0aa134385a159ac0b645d044bb0361cb3
root <root@ip-10-56-75-16.(none)>
2013-07-10 02:04:35 +0000

    [src/pubkey.c] use base16.h

--- src/pubkey.c
+++ src/pubkey.c
@@ -1,19 +1,20 @@
 #define USAGE "Usage: pubkey /path/to/seckey\n"
 #include <nacl/crypto_scalarmult_curve25519.h>
 #include <string.h>
-#include <stdio.h>
+#include "base16.h"
 
 int main(int argc, char **argv) {
  if (argc>1) {
-  unsigned char pk[32];
+  unsigned char pk[64];
   unsigned char sk[32];
   int n;
   if (((n=open(argv[1],0))<0)||(read(n,sk,32)<32)||(close(n)<0)) return 64;
   crypto_scalarmult_curve25519_base(pk,sk);
-  memset(sk,0,32);
-  printf("pubkey: ");
-  for (n=0;n<32;++n) printf("%02x",pk[n]);
-  printf("\n");
+  memcpy(sk,pk,32);
+  if (base16_encode(pk,sk,32)!=64) return 255;
+  if (write(1,"pubkey: ",8)!=8) return 255;
+  if (write(1,pk,64)!=64) return 255;
+  if (write(1,"\n",1)-1) return 255;
   return 0;
  }
  write(2,USAGE,strlen(USAGE));
